a84ee29843c403ef23a7020f0ed742a96777215b
Javeedsly <cavidsly@gmail.com>
2025-12-23 20:56:40 +0400

    ....

--- MMS.Web/Controllers/HomeController.cs
+++ MMS.Web/Controllers/HomeController.cs
@@ -1,27 +1,29 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using MMS.Core.Entities; // AppUser-in yerləşdiyi yer
+using MMS.Core.Entities;
 using System.Linq;
 
 namespace MMS.Web.Controllers
 {
     public class HomeController : Controller
     {
-        // 1. _userManager obyektini elan edin
         private readonly UserManager<AppUser> _userManager;
 
-        // 2. Constructor vasitəsilə obyektin mənimsədilməsini (Dependency Injection) təmin edin
         public HomeController(UserManager<AppUser> userManager)
         {
             _userManager = userManager;
         }
 
+        // --- BU HİSSƏ ÇATIŞMIRDI (SƏHVİN SƏBƏBİ) ---
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // -------------------------------------------
+
         public IActionResult Graduates()
         {
-            // Bütün istifadəçiləri bazadan çəkirik
             var users = _userManager.Users.ToList();
-
-            // Verilənləri View-a göndəririk
             return View(users);
         }
     }
